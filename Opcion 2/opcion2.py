c87da0e20268188cfb50d67d150ae9c01be3dd6f
Xbolanos <xbolanosfonseca@gmail.com>
2018-05-05 19:00:12 -0600

    Opcion 3 Lista

--- Opcion 2/opcion2.py
+++ Opcion 2/opcion2.py
@@ -1,5 +1,16 @@
 from pyspark import SparkContext 
-from pyspark.sql import SQLContext 
 import pandas as pd
-myrdd = sc.textFile("artistToGaguiel.csv").map(lambda line: line.split(","))
-print(myrdd)
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+sc =SparkContext()
+sqlContext = SQLContext(sc)
+df = sqlContext.read.load('artistToGaguiel.csv', 
+                          format='com.databricks.spark.csv', 
+                          header='false', 
+                          inferSchema='true')
+
+rows=df.take(df.count())
+
+list_temp=rows[0][0].split('\"')
+print(list_temp[1])
+
--- Opcion 3/js/example.js
+++ Opcion 3/js/example.js
@@ -1,8 +1,8 @@
-function loadJSON(callback) {
+function loadJSON(callback , file) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'tabledata.json', true);
+    xobj.open('GET', file, true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
 
@@ -14,9 +14,108 @@ function loadJSON(callback) {
     xobj.send(null);
 
 }
-//var myList =[{"id":1,"first_name":"Ardelis","last_name":"Spenceley","email":"aspenceley0@npr.org","gender":"Female","ip_address":"131.208.29.28"},
-//{"id":2,"first_name":"Jacques","last_name":"Gorch","email":"jgorch1@earthlink.net","gender":"Male","ip_address":"198.25.227.146"}
-//]
+
+function generateGraph(idUser){
+  console.log("Me han llamado");
+  loadJSON(function(response) {
+    // Do Something with the response e.g.
+    jsonresponse = JSON.parse(response);
+    
+    var x=get_keys(jsonresponse);
+   
+    var id=x.shift();
+    var y_values=[];
+ 
+      for (var i = 0; i < jsonresponse.length; i++) {
+        if(jsonresponse[i][id]==idUser){
+          for (var j = 0; j < x.length; j++) {
+            y_values.push(jsonresponse[i][x[j]]);
+
+          }
+    }
+  }
+  generatePlotly(x,y_values);
+
+
+  
+  
+
+    
+  },'graphic.json');  
+}
+
+function generatePlotly(x,y){
+  console.log(x);
+  console.log(y);
+  (function() {
+var d3 = Plotly.d3;
+
+var WIDTH_IN_PERCENT_OF_PARENT = 10,
+    HEIGHT_IN_PERCENT_OF_PARENT = 50;
+
+var gd3 = d3.select('body')
+    .append('div')
+    .style({
+        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+        'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+
+
+        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+        'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+    });
+
+
+    TESTER = document.getElementById('graph');
+    /* Current Plotly.js version */
+    console.log( Plotly.BUILD );    
+
+    var gd = gd3.node();
+
+    data=[{
+        type: 'bar',
+        x: x,
+        y: y,
+        marker: {
+            color: '#C8A2C8',
+            line: {
+                width: 0.5
+            }
+        }
+    }]
+    layout={
+        title: 'INCOME',
+        autosize: false,
+        width: 550,
+        font: {
+            size: 16
+        }
+    }
+    Plotly.newPlot(TESTER, data,layout);
+
+    window.onresize = function() {
+        Plotly.Plots.resize(gd);
+    };
+
+    })();
+}
+
+function get_keys(json) {
+  var col = [];
+  for (var i = 0; i <  json.length; i++) {
+      for (var key in  json[i]) {
+          if (col.indexOf(key) === -1) {
+              
+              col.push(key);
+          }
+      }
+  }
+  return col;
+ 
+}
+
+
+
+
 var myList=[]
 loadJSON(function(response) {
   // Do Something with the response e.g.
@@ -24,61 +123,77 @@ loadJSON(function(response) {
 
   // Assuming json data is wrapped in square brackets as Drew suggests
   myList=jsonresponse;
-  console.log(myList);
   
+  var col = get_keys(jsonresponse);
 
 
-  
-});  
+  var table = document.createElement("table");
+  var tr = table.insertRow(-1);                   // TABLE ROW.
 
-
-$(document).ready(function(e) {
-  $.getJSON( "tabledata.json" , function( result ){
-      myList=result;
-      console.log(myList);
-      
-    });
-});
-
-// Builds the HTML Table out of myList.
-function buildHtmlTable(selector) {
-
-  var columns = addAllColumnHeaders(myList, selector);
-
-  for (var i = 0; i < myList.length; i++) {
-    var row$ = $('<tr/>');
-    for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-      var cellValue = myList[i][columns[colIndex]];
-      if (cellValue == null) cellValue = "";
-      row$.append($('<td/>').html(cellValue));
-    }
-    $(selector).append(row$);
-  }
-}
-
-
-// Adds a header row to the table and returns the set of columns.
-// Need to do union of keys from all records as some records may not contain
-// all records.
-function addAllColumnHeaders(myList, selector) {
-  var columnSet = [];
-  var headerTr$ = $('<tr/>');
-
-   
-   
-  for (var i = 0; i < myList.length; i++) {
-    var rowHash = myList[i];
-    for (var key in rowHash) {
-      if ($.inArray(key, columnSet) == -1) {
-        columnSet.push(key);
-        headerTr$.append($('<th/>').html(key));
+      for (var i = 0; i < col.length; i++) {
+          var th = document.createElement("th");      // TABLE HEADER.
+          th.innerHTML = col[i];
+          tr.appendChild(th);
+          tr.setAttribute('class', 'tbl-header');  
+          
       }
+
+  var th = document.createElement("th");      // TABLE HEADER.
+  th.innerHTML = " ";
+  tr.appendChild(th);
+  var divContainerHeader = document.getElementById("showHeaders");
+
+  divContainerHeader.innerHTML = "";
+  divContainerHeader.appendChild(table);
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < myList.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+
+            tabCell.innerHTML = myList[i][col[j]];
+        }
+
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute("class","btn btn-success");
+        btn.setAttribute("id",myList[i][col[0]]);
+        tr.setAttribute("align","center");
+        btn.setAttribute("data-toggle","modal");
+        btn.setAttribute("data-target","#myModal");
+    
+        
+        var t = document.createTextNode("Income");
+        btn.appendChild(t);
+
+        tr.appendChild(btn);
+        var id_temp=myList[i][col[0]];
+        document.getElementById(id_temp).onclick = function() {
+          console.log(this.id);
+          generateGraph(this.id);
+
+        };
+
     }
-  }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showData");
+    table.setAttribute('class', '');  
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+    
+
+  
+
+
+  
+},'tabledata.json');  
 
 
 
-$(selector).append(headerTr$);
-  return columnSet;
-}
+
+// Some info about viewport percentages:
+// http://stackoverflow.com/questions/1575141/make-div-100-height-of-browser-window
+
 
